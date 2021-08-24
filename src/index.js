515a03c5f0007e43a8f32a07896061f8adb145dd
Raccoon <nobllizdc@gmail.com>
2021-08-24 15:00:54 +0900

    fix: Remove debugging code

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import Raccoonact from './components/core/Raccoonact.js';
 import { createElement } from '../src/utils.js';
 
 const rootElement = document.getElementById('root');
-console.log(App());
 Raccoonact.render(<App />, rootElement);
